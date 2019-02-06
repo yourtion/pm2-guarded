8f84d2dc255c2d5fd15ae22ace4b1867a47d2f00
YourtionGuo <yourtion@gmail.com>
2019-02-06 20:28:23 +0800

    feat: add config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import { initPmx } from "./pmx";
 
 const config = initPmx();
-console.log(config);
-
-setInterval(() => {}, 100);
+if (config.influxdb) {
+  console.log(config);
+  setInterval(() => {}, 1000);
+} else {
+  // run empty loop
+  console.log("Run `pm2 set pm2-guarded:influxdb 666` to start monit");
+  setInterval(() => {}, 60000);
+}
