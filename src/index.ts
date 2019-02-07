a35f50ec9a948c329234a6eea03a02241c088024
YourtionGuo <yourtion@gmail.com>
2019-02-07 19:32:28 +0800

    feat: add influx and send system info

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,44 @@
 import { initPmx } from "./pmx";
+import { InfluxDB, IPoint } from "influx";
+import SystemInfo from "./utils/sysinfo";
+import Logger from "./utils/log";
 
 const config = initPmx();
+const logger = new Logger(config.debug);
+
+// let EVENTS: Record<string, any>[] = [];
+let SEDNING = false;
+
 if (config.influxdb) {
   console.log(config);
-  setInterval(() => {}, 1000);
+
+  const fetchInterval = config.fetchInterval || 1000;
+  const sendInterval = config.sendInterval || 5000;
+
+  const system = new SystemInfo(fetchInterval);
+  const influx = new InfluxDB(config.influxdb);
+
+  setInterval(async function() {
+    if (SEDNING) return;
+    logger.debug("Start SEDNING");
+    SEDNING = true;
+    try {
+      const sysInfo = system.getInfo();
+      logger.debug(sysInfo);
+      const data: IPoint[] = [];
+      data.push({ measurement: "sysinfo", tags: { host: system.hostname }, fields: sysInfo, timestamp: new Date() });
+      await influx.writePoints(data);
+    } catch (error) {
+      logger.error(error);
+    } finally {
+      SEDNING = false;
+    }
+    logger.debug("End SEDNING");
+  }, sendInterval);
 } else {
   // run empty loop
-  console.log("Run `pm2 set pm2-guarded:influxdb 666` to start monit");
+  logger.info("Run `pm2 set pm2-guarded:influxdb http://user:pass@host:port/db` to start monit");
+  logger.info("Run `pm2 set pm2-guarded:fetchInterval 1000` to set info fetch interval");
+  logger.info("Run `pm2 set pm2-guarded:sendInterval 5000` to set data send interval");
   setInterval(() => {}, 60000);
 }
