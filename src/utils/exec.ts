a244a7356447f67c4063d04ac276b874610bbcb6
YourtionGuo <yourtion@gmail.com>
2019-02-08 16:33:28 +0800

    fix: exec bug

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -5,10 +5,11 @@ const opts = {
     LANG: "en_US.UTF-8",
   }),
   // give it anyway since ignored by non-windows systems
+  maxBuffer: 5 * 1024 * 1024,
   windowsHide: true,
 };
 
-export default function exec(cmd: string) {
+export default function exec(cmd: string): Promise<string> {
   return new Promise((resolve, reject) => {
     childProcess.exec(cmd, opts, (err, stdout, stderr) => {
       if (err) return reject(err);
